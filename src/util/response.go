06088a343ed3ba59358ee008d9b398c1e8503c3d
ludw1gj <19744067+ludw1gj@users.noreply.github.com>
2018-12-03 10:39:07 +1100

    Removed default comments

--- src/util/response.go
+++ src/util/response.go
@@ -7,10 +7,6 @@ import (
 	"github.com/ludw1gj/mathfever/src/mathematics"
 )
 
-// Response is of type APIGatewayProxyResponse since we're leveraging the
-// AWS Lambda Proxy Request functionality (default behavior)
-//
-// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
 
 type CalculationResponse struct {
