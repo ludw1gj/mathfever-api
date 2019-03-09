9c761169336eec4dc2edc9e120c6e900cd213d6a
ludw1gj <19744067+ludw1gj@users.noreply.github.com>
2019-03-10 10:47:44 +1100

    🎨 Cleaned up some code

--- src/api/validate.go
+++ src/api/validate.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 )
 
+const max = 999999999999
+
 func validateBinary(binary string) error {
 	b, err := strconv.ParseInt(binary, 2, 0)
 	if err != nil || b < 1 || len(binary) > 64 {
@@ -16,7 +18,7 @@ func validateBinary(binary string) error {
 
 func validatePositiveInt(numbers ...int) error {
 	for _, n := range numbers {
-		if n < 1 || n > 999999999999 {
+		if n < 1 || n > max {
 			return fmt.Errorf("please enter a valid decimal number, greater than 0 and less than "+
 				"1'000'000'000'000. Incorrect input: %d", n)
 		}
@@ -26,17 +28,12 @@ func validatePositiveInt(numbers ...int) error {
 
 func validateFloat(positiveOnly bool, floats ...float64) error {
 	for _, n := range floats {
-		switch positiveOnly {
-		case false:
-			if n > 999999999999 {
-				return fmt.Errorf("please enter a valid decimal number, greater than 0 and less than "+
-					"1'000'000'000'000: %f", n)
-			}
-		case true:
-			if n < 1 || n > 999999999999 {
-				return fmt.Errorf("please enter a valid decimal number, greater than 0 and less than "+
-					"1'000'000'000'000: %f", n)
-			}
+		if positiveOnly && n < 1 {
+			return fmt.Errorf("please enter a valid decimal number, greater than 0")
+		}
+		if n > max {
+			return fmt.Errorf("please enter a valid decimal number, must be less than "+
+				"1'000'000'000'000: %f", n)
 		}
 	}
 	return nil
